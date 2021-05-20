2bb99b2d55ab475b82288e0fdd881c698d364a0f
ulvi macid <ulvizm@code.edu.az>
2021-05-20 14:57:36 +0400

    Add project files.

---
+++ Digiuth/Controllers/AuthController.cs
@@ -0,0 +1,116 @@
+﻿using Digiuth.Models;
+using Digiuth.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Digiuth.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AuthController(UserManager<AppUser> userManager,
+                                SignInManager<AppUser> signInManager,
+                                RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM login)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser appUser = await _userManager.FindByEmailAsync(login.Email);
+
+            if (appUser == null)
+            {
+                ModelState.AddModelError("", "Email or Password is wrong!!");
+                return View(login);
+            }
+
+            var signinResult = await _signInManager.PasswordSignInAsync(appUser, login.Password, true, true);
+
+            if (!signinResult.Succeeded)
+            {
+                ModelState.AddModelError("", "Email or Password is wrong!!");
+                return View(login);
+            }
+
+            if (signinResult.IsLockedOut)
+            {
+                ModelState.AddModelError("", "The account is locked Out");
+                return View(login);
+            }
+
+            var role = (await _userManager.GetRolesAsync(appUser))[0];
+
+            if (role == "Admin")
+            {
+                return RedirectToAction("Index", "Dashboard", new { area = "Admin" });
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM register)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser newUser = new AppUser
+            {
+                FullName = register.FullName,
+                UserName = register.UserName,
+                Email = register.Email
+            };
+
+            IdentityResult identityResult = await _userManager.CreateAsync(newUser, register.Password);
+
+            if (!identityResult.Succeeded)
+            {
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(register);
+            }
+            await _userManager.AddToRoleAsync(newUser, "User");
+
+            await _signInManager.SignInAsync(newUser, true);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        //public async Task CreateRole()
+        //{
+        //    if (!await _roleManager.RoleExistsAsync("Admin"))
+        //    {
+        //        await _roleManager.CreateAsync(new IdentityRole { Name = "Admin" });
+        //    }
+        //    if (!await _roleManager.RoleExistsAsync("User"))
+        //    {
+        //        await _roleManager.CreateAsync(new IdentityRole { Name = "User" });
+        //    }
+        //}
+    }
+}
---
+++ Digiuth/Startup.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Digiuth.DAL;
+using Digiuth.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Digiuth
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(_config["ConnectionString:Default"]);
+            });
+            services.AddIdentity<AppUser, IdentityRole>(identityOptions =>
+            {
+                identityOptions.Password.RequiredLength = 6;
+                identityOptions.Password.RequireNonAlphanumeric = false;
+                identityOptions.Password.RequireLowercase = false;
+                identityOptions.Password.RequireUppercase = false;
+                identityOptions.Password.RequireDigit = true;
+
+                identityOptions.User.RequireUniqueEmail = true;
+
+                identityOptions.Lockout.AllowedForNewUsers = true;
+                identityOptions.Lockout.MaxFailedAccessAttempts = 3;
+                identityOptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseStaticFiles();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
